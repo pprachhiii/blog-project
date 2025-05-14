31df69f578ef8676eeb8757824d3595bf0634a99
Prachi Yadav <prachiiyadav2409@gmail.com>
2025-05-14 23:49:12 +0530

    starting

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Blog!</h1>
+      </header>
+      <main>
+        <p>This is where my blog content will go.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
